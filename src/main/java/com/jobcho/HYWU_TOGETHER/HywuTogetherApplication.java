90359359f280d9f75bf63d65ea762846cb4d6006
urma <syl000126@naver.com>
2022-07-24 21:25:08 +0900

    HYWUTO - first

---
+++ src/main/java/com/jobcho/HYWU_TOGETHER/HywuTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.jobcho.HYWU_TOGETHER;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HywuTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HywuTogetherApplication.class, args);
+	}
+
+}
